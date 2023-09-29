b03515ec9e249ad9e3ff0346d11f9e607efb1878
mosmar99 <47375043+mosmar99@users.noreply.github.com>
2023-09-29 13:57:49 +0100

    completed and showed lab

---
+++ labs/lab4/calc_time.py
@@ -0,0 +1,14 @@
+
+f_tim = 84*(10**(6))
+f_sys = 1*(10**(6))
+
+def np(f_tim, f_sys):
+    return f_tim / f_sys 
+
+np_var = np(f_tim, f_sys)
+print(np_var)
+
+psc = 1
+ARR = 41
+
+print((psc+1)*(ARR+1))
